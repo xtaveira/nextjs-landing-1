bdc2eddbaeea3cff5e2f5fa56a20fe9bceb4c42f
Davi Taveira <davimtaveira@gmail.com>
2023-12-23 07:14:51 -0300

    First Structure

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main>
+      {/* Capture reader's attention first */}
+      <section>Hero</section>
+
+      {/* Describe the problem your header has and how you will adress it */}
+      <section>Text</section>
+
+      {/* Sell you points, Features + Benefits */}
+      <section>Features</section>
+
+      {/* Testimonials from your service */}
+      <section>Social Proof</section>
+
+      {/* CTA is the easy possibiliry for readers to engage */}
+      <section>Call to Action</section>
+
+    </main>
+  )
+}
+
+export default Home
